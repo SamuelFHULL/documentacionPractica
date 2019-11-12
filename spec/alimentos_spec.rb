973cfec2134403d5ebbb39e97cedea93e9f061f0
usuario <alu0101033112@ull.edu.es>
2019-11-12 15:36:13 +0000

    Falla prueba de creacion de objeto

--- spec/alimentos_spec.rb
+++ spec/alimentos_spec.rb
@@ -1,9 +1,12 @@
 RSpec.describe Alimentos do
+  
+  before :all do
+    @carne_vaca = Alimento.new("Carne de vaca", 21.1, 0.0, 3.1, 50.0, 164.0)
+  end
   it "has a version number" do
     expect(Alimentos::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "La clase alimento tiene un nombre" do
+	  expect(@carne_vaca.nombre).to eq("Carne de vaca")	  
   end
 end
