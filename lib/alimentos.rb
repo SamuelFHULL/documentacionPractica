369b0857f615225d06f5426d00a3a4844d625521
usuario <alu0101033112@ull.edu.es>
2019-11-12 15:57:46 +0000

    Superada prueba gases

--- lib/alimentos.rb
+++ lib/alimentos.rb
@@ -3,11 +3,12 @@
 #module Alimentos
  # class Error < StandardError; end
   class Alimento
-	  attr_reader :nombre
+	  attr_reader :nombre, :gei
 	  def initialize(nombre, proteinas, carbohidratos, lipidos, gei, terreno)
 		  @nombre = nombre
 		  @proteinas = proteinas
 		  @carbohidratos = carbohidratos
+		  @gei = gei
 		  
 
 	  end
