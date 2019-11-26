0fde8acbad1a4a13d604eb09bf236c7c533731aa
usuario <alu0101033112@ull.edu.es>
2019-11-26 11:26:45 +0000

    Falla prueba comprobar que un alimento sea menor qu eotro

--- spec/alimentos_spec.rb
+++ spec/alimentos_spec.rb
@@ -58,6 +58,12 @@ RSpec.describe Alimentos do
   it "Existe metodo impacto ambiental" do
 	  expect(@carne_vaca.impactoAmbiental(@lista_alimentos, @lista_cantidades,0)).to eq(1559.7199999999998)
   end
+
+  context "Se pueden comparar alimentos segun su eficiencia energetica" do
+    it "Existe metodo para comparar que un alimento sea menor que otro" do
+      expect(@cafe<@carne_vaca).to eq(true)
+    end
+  end
  
 
   
