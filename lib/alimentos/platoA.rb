9c2a8b6c59d449c1c0819531cd4fe847b6c5292c
usuario <alu0101033112@ull.edu.es>
2019-11-26 22:57:31 +0000

    Superada prueba existe conjunto de cantidades para plato

--- lib/alimentos/platoA.rb
+++ lib/alimentos/platoA.rb
@@ -4,7 +4,7 @@ class PlatoA
 	def initialize (nombre, lista_alimentos, lista_cantidades)
 		@nombre = nombre
 		@lista_alimentos = lista_alimentos
-		#@lista_cantidades = lista_cantidades
+		@lista_cantidades = lista_cantidades
 	end
 end
 end
